20fa90bd4c910ad922dbfa8c40d121b272687a80
Thomas Tanon <Tpt@users.noreply.github.com>
2016-01-12 11:00:10 +0100

    Update PPP NLP backend url

--- www/query.php
+++ www/query.php
@@ -4,5 +4,5 @@ namespace PPP\WikidataSparql;
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-$entryPoint = new WikidataSparqlEntryPoint( 'http://askplatyp.us:9002/' );
+$entryPoint = new WikidataSparqlEntryPoint( 'http://grammatical.backend.askplatyp.us/' );
 $entryPoint->exec();
